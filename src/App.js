549a3aa5463575cedc8cc5839fa8754975e9a696
Shujaat Bakhsh <shujaatbakhsh25@gmail.com>
2020-01-13 01:30:36 +0530

    netlify test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload back loloolol kiololpl.
+          Edit <code>src/App.js</code> and save to reload back loloolol kiololpl nnnnn.
         </p>
         <a
           className="App-link"
