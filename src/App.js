b89b96d2ef230750e6697d841a804fc11814f3e7
Shujaat Bakhsh <shujaatbakhsh25@gmail.com>
2020-01-14 21:06:29 +0530

    misc

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload back loloolol kiololpl nnnnn. Anshul is my friend.
-          aur bhai kaisa hai ?
+          Makespace front end component round....
         </p>
         <a
           className="App-link"
